421913c90aab8eaf6953314dcdc5f383d35901f2
Aishwary200 <amritakeshri3059@gmail.com>
2021-06-28 23:29:29 +0530

    Know the Indian states

--- screens/StateDetail.js
+++ screens/StateDetail.js
@@ -3,123 +3,145 @@ export const myObj = [
         stateName: "Andhra Pradesh", capital: "Amaravati",
         Area: '160,205 km²', date: '1 Nov. 1956', food: 'Pootharekulu',
         site: 'Birla Mandir, Bhavani Island, Venkateswara Temple, Golkonda fort',
-        lan: 'Telugu', pop: '5.44 Crores'
+        lan: 'Telugu', pop: '5.44 Crores', cm: 'Shri YS Jagan Mohan Reddy',
+        gov: 'Shri Biswa Bhusan Harichandan'
     }, {
         stateName: "Arunachal Pradesh", capital: "Itanagr",
         Area: '83,743 km²', date: '20 Feb. 1987', food: 'Thukpa',
         site: 'Tawang, Bombdila, Lohit District, Ziro',
-        lan: 'Nishi', pop: '15.92 Lakhs'
+        lan: 'Nishi', pop: '15.92 Lakhs', cm: '	Shri Pema Khandu',
+        gov: 'Brig. (Dr.) B. D. Mishra (Retd.)'
     }, {
         stateName: "Assam", capital: "Dispur",
         Area: '78,438 km²', date: 'April 7, 1937', food: 'Papaya Khar',
         site: 'Kaziranga National Park, Pobitora Wildlife Sanctuary, Kamakhya Temple, Uma Nanda Temple',
-        lan: 'Assamese', pop: '3.60 Crores'
+        lan: 'Assamese', pop: '3.60 Crores', cm: 'Shri Himanta Biswa Sarma',
+        gov: 'Prof. Jagdish Mukhi'
     }, {
         stateName: "Bihar", capital: "Patna",
         Area: '94,163 km²', date: '22 March 1912', food: 'Litti Chokha',
         site: 'Mahabodhi Temple, Rajgir, Nalanda, Vikramshila, Great Buddha Statue, GolGhar, Takht Sri Harimandir Ji Patna Sahib',
-        lan: 'Bhojpuri, Magahi, Maithili', pop: '12.70 Crores'
+        lan: 'Bhojpuri, Magahi, Maithili', pop: '12.70 Crores', cm: 'Shri Nitish Kumar',
+        gov: 'Shri Phagu Chauhan'
     }, {
         stateName: "Chhattisgarh", capital: "Raipur",
         Area: '135,191 km²', date: '1 Nov. 2000', food: 'Chila',
         site: 'Chittrakote WaterFalls, Bhoramdeo Temple, Maikal Hills, Kanger Vally National Park',
-        lan: 'Chhattisgarhi', pop: '2.98 Crores'
+        lan: 'Chhattisgarhi', pop: '2.98 Crores', cm: 'Shri Bhupesh Baghel',
+        gov: 'Sushri Anusuiya Uikey'
     }, {
         stateName: "Goa", capital: "Panaji",
         Area: '3,702 km²', date: '30 May. 1987', food: 'Fish Curry',
         site: 'Agonda Beach, Basilica of Bom Jesus, Palolem Beach, Mangueshui Temple,Baga Beach, Grand Island',
-        lan: 'Konkani', pop: '16 Lakhs'
+        lan: 'Konkani', pop: '16 Lakhs', cm: 'Shri Pramod Sawant',
+        gov: 'Shri Bhagat Singh Koshyari'
     }, {
         stateName: "Gujarat", capital: "Gandhinagar",
         Area: '196,024 km²', date: '1 May. 1960', food: 'Dhokla',
         site: 'Sabarmati Ashram, Lakhota Museum, Bhavnath Mahadev, Saputara Hills, Statue of Unity, Dwarka Temple, Somnath Temple',
-        lan: 'Gujarati', pop: '6.43 Crores'
+        lan: 'Gujarati', pop: '6.43 Crores', cm: 'Shri Vijaybhai R. Rupani',
+        gov: 'Shri Acharya Dev Vrat'
     }, {
         stateName: "Haryana", capital: "Chandigarh",
         Area: '44,212 km²', date: '1 Nov. 1966', food: 'Bajre Ki Khichdi',
         site: 'Badhkal Lake, Morni Hills, Dumdama Lake, Sultanpur Bird Sanctuary',
-        lan: 'Haryanvi, Mewati, Punjabi', pop: '2.85 Crores'
+        lan: 'Haryanvi, Mewati, Punjabi', pop: '2.85 Crores', cm: 'Shri Manohar Lal Khattar',
+        gov: 'Shri Satyadeo Narain Arya'
     }, {
         stateName: "Himachal Pradesh", capital: "Shimla",
         Area: '55,673 km²', date: '25 Jan. 1971', food: 'Dham',
         site: 'Shimla, Manali, Dal Lake, Khajjiar (Mini Switzerland of India)',
-        lan: 'Dogri, Pahari, Hindi', pop: '75 Lakhs'
+        lan: 'Dogri, Pahari, Hindi', pop: '75 Lakhs', cm: 'Shri Jairam Thakur',
+        gov: 'Shri Bandaru Dattatraya'
     }, {
         stateName: "Jharkhand", capital: "Ranchi",
         Area: '79,714 km²', date: '15 Nov. 2000', food: 'Dhooska',
         site: 'Hazaribagh Wildlife Sanctuary, Parasnath, Deoghar, Canary Hill, Jamshedpur, Jubilee park, Hundru Falls(Ranchi)',
-        lan: 'Magahi, Maithili, Mundari, Santhali', pop: '3.92 Crores'
+        lan: 'Magahi, Maithili, Mundari, Santhali', pop: '3.92 Crores', cm: 'Shri Hemant Soren',
+        gov: 'Shrimati Droupadi Murmu'
     },
     {
         stateName: "Karnataka", capital: "Bengaluru",
         Area: '191,791 km²', date: '1 Nov. 1956', food: 'Bisi Bele Bath',
         site: 'Keshava Temple, Coorg Hills, Mysore Palace, Gol Gumbaz',
-        lan: 'Kannada', pop: '6.83 Crores'
+        lan: 'Kannada', pop: '6.83 Crores', cm: 'Sri B. S. Yediyurappa',
+        gov: 'Shri Vajubhai Vala'
     }, {
         stateName: "Kerala", capital: "Thiruvanathpuram",
         Area: '33,406,061 km²', date: '1 Nov. 1956', food: 'Appam',
         site: 'Munnar, Kumarakom, Kerala Folklore Theatre & Museum, Arthirampally,Kerala Backwaters, Cheraman Juma Masjid',
-        lan: 'Malayalam', pop: '3.60 Crores'
+        lan: 'Malayalam', pop: '3.60 Crores', cm: 'Shri Pinarayi Vijayan',
+        gov: 'Shri Arif Mohammed Khan'
     },
     {
         stateName: "Madhya Pradesh", capital: "Bhopal",
         Area: '308,245 km²', date: '1 Nov. 1956', food: 'Bhutte Ka Kees',
         site: 'Khajuraho Temple, Buddhist Monuments, Taj ul Masajid, Orchha Fort, Taj Ul Masjid',
-        lan: 'Hindi, Malvi, Nimadi', pop: '8.70 Crores'
+        lan: 'Hindi, Malvi, Nimadi', pop: '8.70 Crores', cm: 'Shri Shivraj Singh Chouhan',
+        gov: 'Smt. Anandiben Patel'
     },
     {
         stateName: "Maharashtra", capital: "Mumbai",
         Area: '307,713 km²', date: '1 May. 1960', food: 'Misal Pav',
         site: 'Gateway of India, Mumbadevi Temple, Panhala Hills, Ajanta-Ellora Cavas, Jain temple, Haji Ali Mosque',
-        lan: 'Marathi', pop: '12.43 Crores'
+        lan: 'Marathi', pop: '12.43 Crores', cm: 'Shri Uddhav Thackeray',
+        gov: 'Shri Bhagat Singh Koshyari'
     },
     {
         stateName: "Manipur", capital: "Imphal",
         Area: '22,327 km²', date: '21 Jan. 1972', food: 'Kangshoi',
         site: 'Sri Sri Govindajee Temple, Loktak Lake, Sirohi National Park, Manipur State Museum',
-        lan: 'Meitei', pop: '32 Lakhs'
+        lan: 'Meitei', pop: '32 Lakhs', cm: 'Shri N. Biren Singh',
+        gov: 'Dr. Najma Heptulla'
     },
     {
         stateName: "Meghalaya", capital: "Shillong",
         Area: '22,429 km²', date: '21 Jan. 1972', food: 'Jadoh',
         site: 'Butterfly Museum, Umium Lake, Mawsmai Caves, Elephant Falls',
-        lan: 'Khasi, Jaintia, Garo, English', pop: '34 Lakhs'
+        lan: 'Khasi, Jaintia, Garo, English', pop: '34 Lakhs', cm: 'Shri Conrad Kongkal Sangma',
+        gov: 'Shri Satya Pal Malik'
     },
     {
         stateName: "Mizoram", capital: "Aizawl",
         Area: '21,081 km²', date: '20 Feb. 1987', food: 'Misa Mach Poora',
         site: 'Durtlang Hills, Champhai, Dampa Wildlife Sanctuary,Vantawng Falls',
-        lan: 'Mizo, English', pop: '12.55 Lakhs'
+        lan: 'Mizo, English', pop: '12.55 Lakhs', cm: 'Shri Pu Zoramthanga',
+        gov: 'Shri P.S. Sreedharan Pillai'
     },
     {
         stateName: "Nagaland", capital: "Kohima",
         Area: '16,579 km²', date: '1 Dec. 1963', food: 'Pork with Bamboo Shoot',
         site: 'Naga Heritage Village, Kohima War Cemetery, Kisama Heritage Village, Kohima Museum',
-        lan: 'Nagamese', pop: '23 Lakhs'
+        lan: 'Nagamese', pop: '23 Lakhs', cm: 'Shri Neiphiu Rio',
+        gov: 'Shri R. N. Ravi'
     },
     {
         stateName: "Odisha", capital: "Bhubaneswar",
         Area: '155,707 km²', date: '1 April 1936', food: 'Chenna Poda',
         site: 'Lingaraja Temple, Hanuman Vatika, Mahanadi Barrage, Puri Beach, Jagannath Temple,Ajmer Sharif Dargah',
-        lan: 'Odia', pop: '4.69 Crores'
+        lan: 'Odia', pop: '4.69 Crores', cm: 'Shri Naveen Patnaik',
+        gov: 'Prof. Ganeshi Lal'
     },
     {
         stateName: "Punjab", capital: "Chandigarh",
         Area: '50,362 km²', date: '1 Nov. 1956', food: 'Makki Di Roti & Sarson Da Saag',
         site: 'Golden Temple (Harmandir Sahib), Akal Takht, Rangla Punjab Haveli, Bhatinda Zoological Park',
-        lan: 'Punjabi', pop: '3 Crores'
+        lan: 'Punjabi', pop: '3 Crores', cm: 'Shri Capt. Amarinder Singh',
+        gov: 'Shri V.P. Singh Badnore'
     },
     {
         stateName: "Rajasthan", capital: "Jaipur",
         Area: '342,239 km²', date: '1 Nov. 1956', food: 'Dal Baati Churma',
         site: 'Jaipur – The Pink City, Gadisar Lake, Hawa Mahal, Thar Desert',
-        lan: 'Hindi, Rajasthani', pop: '8.25 Crores'
+        lan: 'Hindi, Rajasthani', pop: '8.25 Crores', cm: 'Shri Ashok Gehlot',
+        gov: 'Shri Kalraj Mishra'
     },
     {
         stateName: "Sikkim", capital: "Gangtok",
         Area: '7,096 km²', date: '16 May. 1975', food: 'Momo',
         site: 'Gangtok, Tsomgo Lake, Ranka Monastery, Do Drul Chorten Stupa',
-        lan: 'Nepali, English, Hindi, Lepcha', pop: '7 Lakhs'
+        lan: 'Nepali, English, Hindi, Lepcha', pop: '7 Lakhs', cm: 'Shri PS Golay',
+        gov: 'Shri Ganga Prasad'
     },
     {
         stateName: "Tamil Nadu", capital: "Chennai",
@@ -127,73 +149,87 @@ export const myObj = [
         date: 'Established in 1773; Madras State was formed in 1950 and renamed as Tamil Nadu on 14 January 1969',
         food: 'Pongal',
         site: 'Brihadeeswarar  Temple, Mahabalipuram monuments, Ooty Botanical gardens, VGP Snow kingdom',
-        lan: 'Tamil', pop: '7.85 Crores'
+        lan: 'Tamil', pop: '7.85 Crores', cm: 'Shri M. K. Stalin',
+        gov: 'Shri Banwarilal Purohit'
     }, {
         stateName: "Telangana", capital: "Hyderabad",
         Area: '112,077 km²', date: '2 Jun. 2014', food: 'Biryani',
         site: 'Charminar, Galconda Fort, Ramoji Film City, Pakhal lake, Makkah Masjid',
-        lan: 'Telugu', pop: '3.98 Crores'
+        lan: 'Telugu', pop: '3.98 Crores', cm: 'Shri K Chandrasekhar Rao',
+        gov: 'Dr. Tamilisai Soundararajan'
     },
     {
         stateName: "Tripura", capital: "Agartala",
         Area: '10,486 km²', date: '21 Jan. 1972', food: 'Chakhwi',
         site: 'Iskcon Radha-Govinda Mandir, Neermahal Palace, Unakoti,Tripura Sundari Temple',
-        lan: 'Bengali, Tripuri', pop: '42 Lakhs'
+        lan: 'Bengali, Tripuri', pop: '42 Lakhs', cm: 'Shri Biplab Kumar Deb',
+        gov: 'Shri Ramesh Bais'
     }, {
         stateName: "Uttar Pradesh", capital: "Lucknow",
         Area: '240,928 km²', date: '1 April 1937', food: 'Tunde Ke Kebabs',
         site: 'Taj Mahal, Agra Fort, Bara Imambara, Kashi Vishwanath Temple, Varanasi, Nagina Masjid, Jamia Masjid',
-        lan: 'Hindi, Awadhi, Brij', pop: '24 Crores'
+        lan: 'Hindi, Awadhi, Brij', pop: '24 Crores', cm: '	Shri Yogi Aditya Nath',
+        gov: 'Smt. Anandiben Patel'
     }, {
         stateName: "Uttarakhand", capital: "Dehradun (Winter capital) , Gairsain (Summer Capital)",
         Area: '53,483 km²', date: '9 Nov. 2000', food: 'Kafuli',
         site: 'Nainital, Gun Hill, Badrinath Temple, Piran Kaliyar Sharif, Kedarnath Temple',
-        lan: 'Hindi, Garhwali, Kumaoni, Jaunsari', pop: '1.14 Crores'
+        lan: 'Hindi, Garhwali, Kumaoni, Jaunsari', pop: '1.14 Crores', cm: 'Shri Tirath Singh Rawat',
+        gov: 'Smt. Baby Rani Maurya'
     }, {
         stateName: "West Bengal", capital: "Kolkata",
         Area: '88,752 km²', date: '1 Nov. 1956', food: 'Kosha Mangsho',
         site: 'Darjeeling Mountain Hills, Digha Sea Beach, Murshidabad Historical City, Kurseong',
-        lan: 'Bengali', pop: '10 Crores'
+        lan: 'Bengali', pop: '10 Crores', cm: 'Km. Mamata Banerjee',
+        gov: 'Shri Jagdeep Dhankhar'
     }, {
         unionName: "Andaman and Nicobar Islands", capital: "Port Blair",
         Area: '8,249 km²', date: '1 Nov. 1956', food: 'Fish Curry, Seafood',
         site: 'Radhanagar Beach, Havelock Island, Cellular Jail, Barren Island',
-        lan: 'Bengali, Andamanese, Tamil, Telgu, Malayalam, Hindi', pop: '4.2 Lakhs'
+        lan: 'Bengali, Andamanese, Tamil, Telgu, Malayalam, Hindi', pop: '4.2 Lakhs',
+        gov: ''
     }, {
         unionName: "Chandigarh", capital: "Chandigarh",
         Area: '114 km²', date: '1 Nov. 1956', food: 'Chole bhature',
         site: 'Rock Garden of chandigarh, Sukhna Lake, Fun City (Water amusement park), Rose garden',
-        lan: 'Punjabi, Hindi', pop: '11.69 Lakhs'
+        lan: 'Punjabi, Hindi', pop: '11.69 Lakhs',
+        gov: ''
     }, {
         unionName: "Dadra and Nagar Haveli and Daman & Diu", capital: "Daman",
         Area: '603 km²', date: '26 Jan. 2020', food: 'Papyri food',
         site: 'Swaminarayan Temple, Dudhani Lake, Tribal Museum, Hirwavan garden, Diu Fort, Gangeshwar Temple, Nagoa Beach, St. Paul’s Church',
-        lan: 'Gujarati, Hindi', pop: '6.20 Lakhs'
+        lan: 'Gujarati, Hindi', pop: '6.20 Lakhs',
+        gov: ''
     },
     {
         unionName: "Delhi", capital: "New Delhi",
         Area: '1,484 km²', date: '9 May. 1905', food: 'Paratha, Chole bhature',
         site: 'Red Fort, Lotus Temple, India Gate, Humayun’s Tomb, Raj Ghat, Qutub Minar, Jama masjid, Rashtrapati Bhavan, National War Memorial, Quwwat Ul Islam Mosque,Jamali Kamali Mosque',
-        lan: 'Hindi, Urdu', pop: '1.90 Crores'
+        lan: 'Hindi, Urdu', pop: '1.90 Crores', cm: 'Shri Arvind Kejriwal',
+        gov: ''
     }, {
         unionName: "Lakshadweep", capital: "Kavaratti",
         Area: '32.62 km²', date: '1 Nov. 1956', food: 'Seafood',
         site: 'Kavaratti Island, Amindivi Island, Agatti Island, Lighthouse',
-        lan: 'Malayalam, Jeseri, Mahl', pop: '66.15 Thousand'
+        lan: 'Malayalam, Jeseri, Mahl', pop: '66.15 Thousand',
+        gov: ''
     }, {
         unionName: "Puducherry", capital: "Pondicherry",
         Area: '479 km²', date: '1 Nov. 1954', food: 'Seafood',
         site: 'Seaside Promenade, Church of the Sacred Heart of Jesus, Paradise Beach, Pondicherry Botanical Garden',
-        lan: 'Tamil, Telgu, English, French', pop: '14.32 Lakhs'
+        lan: 'Tamil, Telgu, English, French', pop: '14.32 Lakhs', cm: 'Shri N. Rangaswamy',
+        gov: ''
     }, {
         unionName: "Jammu and Kashmir", capital: "Srinagar (Summer capital) , Jammu (Winter capital)",
         Area: '125,535 km²', date: '31 Oct 2019', food: 'Rogan Josh',
         site: 'Gulmarg, Tulip Garden, Sonamarg, Dal Lake,Maa Vaishno Devi, Amarnath Temple, Jamia Masjid',
-        lan: 'Kashmiri, Dogri, Hindi, Urdu', pop: '1.38 Crores'
+        lan: 'Kashmiri, Dogri, Hindi, Urdu', pop: '1.38 Crores',
+        gov: ''
     }, {
         unionName: "Ladakh", capital: "Leh",
         Area: '96,701 km²', date: '31 Oct 2019', food: 'Khambir,Yarkhandi Pulao',
         site: 'Khardung La, Karakoram Wildlife Sanctuary, Shey Monastery, Magnet Hill, Tso moriri',
-        lan: 'Ladakhi', pop: '2.90 Lakhs'
+        lan: 'Ladakhi', pop: '2.90 Lakhs',
+        gov: ''
     },
 ]
